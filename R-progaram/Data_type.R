62654ce3694c4923cfd67aa50d1c2ec507c61865
gurubalan077 <115133453+gurubalan077@users.noreply.github.com>
2023-10-30 10:25:53 +0530

    Add files via upload

---
+++ R-progaram/Data_type.R
@@ -0,0 +1,19 @@
+#data type
+num=643267 #numeric
+class(num)
+com=65+7i
+class(com)
+com1=65i #complex
+class(com1)
+yy=98.9
+class(yy)
+gh=688L #integer
+class(gh)
+log=TRUE #logical
+class(log)
+bol=FALSE #logical
+class(bol)
+bs="8675" # character
+class(bs)
+bs_1='754'
+class(bs_1)
